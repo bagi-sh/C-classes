af727d12e41a90f69cebafbf439f0c4d3a06465e
bagi <alisson.alves1628@gmail.com>
2026-06-13 00:07:00 -0300

    feat: Comand CD criado e estavel

--- shellcmds.c
+++ shellcmds.c
@@ -2,10 +2,23 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <string.h>
 
 void exec_cd(char *arg[]) {
     if (arg[1] == NULL) {
-        chdir(getenv("HOME"));
+        char *home[1024] = (getenv("HOME"));
+        if (home != NULL) chdir(home);
     }
-    if (strchr('~', arg)) 
+    elseif (arg[1] == '~') {
+        char *home[1024] = (getenv("HOME"));
+        if (home != NULL) {
+            char way[2048];
+            strcpy(way, home);
+            strcat(way, arg[1] + 1);
+            chdir(way);
+        }
+    }
+    else {
+        chdir(arg[1]);
+    }
 }
